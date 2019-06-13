03f7c1d28be3a52fefb42d79db0a87c715d54e97
ceya <regist.ceya@gmail.com>
2019-06-13 13:12:19 +0900

    base html done

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
   #app
     .l-inner
       header.l-header
-        h1.p-title Nafdas
+        h1.p-headtitle Nafdas
         span.p-subTitle ナフダス ー オフ会用の名札を簡単に作成・印刷するツール
-      .p-inputTwIDWrap
-        label(for="twID") TwitterID
-        input.__input#twID(type="text" placeholder="TwitterIDを入力")
-      
+      main.l-main
+        .p-inputTwID-
+          label(for="twID") TwitterID
+          input.__inputText#twID(type="text" placeholder="TwitterIDを入力")
+        .p-nameTag
+          .__iconBox
+            img.__icon(src="./assets/avatar_default_150x.png")
+          .__nameBox
+            input.__name(type="text" value="なまえをいれてください") 
+          .__idBox
+            input.__id(type="text" value="twitter ID") 
+        .p-buttonArea
+          button.c-btn.p-btnNetPrint ネットプリント
+          button.c-btn.p-btnSave 画像を保存
+        .p-howto
+          h2.__title つかいかた
+          p TwitterIDを入力するだけで名札が作れます！
+      footer.l-footer
 </template>
 
 <script>
@@ -17,6 +31,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+//////////////////////////////////////////////////
+// foundation
+//////////////////////////////////////////////////
+body{
+  //反映されない
+  margin: 0;
+  padding: 0;
+}
 #app{
  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
   font-family:"游ゴシック Medium",YuGothic,YuGothicM,"Hiragino Kaku Gothic ProN","Hiragino Kaku Gothic Pro",メイリオ,Meiryo,sans-serif;
@@ -29,11 +51,26 @@ export default {
   padding: 0;
 }
 
+//////////////////////////////////////////////////
+// Layout
+//////////////////////////////////////////////////
 .l-inner{
   margin:0 auto;
   padding:0 1em;
   width: 100%;
   max-width: 800px;
 }
+
+//////////////////////////////////////////////////
+// Component
+//////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////
+// Project
+//////////////////////////////////////////////////
+
+
 
 </style>
