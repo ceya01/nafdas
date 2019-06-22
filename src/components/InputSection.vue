47974e75e6df0393ca2ecdd706d6b05c91915b8b
ceya <regist.ceya@gmail.com>
2019-06-22 19:04:23 +0900

    読み込みボタンを追加

--- src/components/InputSection.vue
+++ src/components/InputSection.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   section.p-inputSection
     label(for="twID") TwitterID：
-    input.__inputText#twID(type="text" placeholder="TwitterIDを入力" :value="twitterID" 
-    v-on:change ="onChangeInput")
-    button.__submit.c-btn 読込
+    input.__inputText#twID(type="text" placeholder="TwitterIDを入力" v-model="twitterID" 
+    @change ="onChangeInput")
+    button.__btnLoad.c-btn(@click="onClickLoad") 読込
     //-p twID: {{twitterID}}
 </template>
 
@@ -11,13 +11,23 @@
 export default {
   name: 'InputSection',
   computed:{
-    twitterID : function(){
-      return this.$store.getters.twitterID
+    twitterID : {
+      get (){
+        return this.$store.getters.twitterID
+      },
+      set (value){
+        this.$store.commit('setTwitterID', value)
+      }
     }
   },methods:{
     onChangeInput: function(evt){
       console.log('onChangeInput');
-      let twitterID = evt.target.value;
+      // let twitterID = evt.target.value;
+      // this.$store.dispatch('updateTwitterID',twitterID)
+    },
+    onClickLoad: function(evt){
+      console.log('onClickLoad');
+      let twitterID = this.twitterID;
       this.$store.dispatch('updateTwitterID',twitterID)
     }
   }
@@ -30,7 +40,11 @@ export default {
   >.__inputText{
     outline: 1px solid #666;
     padding: 0 0.5em;
+    margin-right: 1em;
     background-color: #fff;
+  }
+  >.__btnLoad{
+    
   }
   @media print{
     display: none;
